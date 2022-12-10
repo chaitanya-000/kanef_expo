e2dd83a6b2bd2e7be893c28b941c63812d9c7b1f
Chaitanya Joshi <Chaitanya99j@gmail.com>
2022-12-10 13:18:31 +0530

    login image refactored

--- src/Screens/Login.tsx
+++ src/Screens/Login.tsx
@@ -1,37 +1,53 @@
 import React from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
 import { Body1, Heading3, Heading5 } from "../atoms/Typography";
 import EmailAddress from "../organisms/EmailAddress";
 import Password from "../organisms/Password";
 import { useNavigation } from "@react-navigation/native";
 import { GreenButton } from "../atoms/GreenButton";
+import {
+  responsiveScreenHeight,
+  responsiveScreenWidth,
+} from "react-native-responsive-dimensions";
 
 const Login = ({ navigation }: any) => {
   return (
-    <View style={styles.container}>
-      <Heading5 style={{ alignSelf: "flex-start" }}>Login</Heading5>
-      <EmailAddress />
-      <Password />
-      <GreenButton height={"8%"} marginTop={"7%"} width={"100%"}>
-        <Body1 style={{ color: "white" }}>Login</Body1>
-      </GreenButton>
-    </View>
+    <>
+      <Image
+        source={require("../../assets/images/LoginImage2x.png")}
+        style={styles.image}
+      />
+      <View style={styles.container}>
+        <Heading5 style={{ alignSelf: "flex-start" }}>Login</Heading5>
+        <EmailAddress />
+        <Password />
+        <GreenButton height={"8%"} marginTop={"7%"} width={"100%"}>
+          <Body1 style={{ color: "white" }}>Login</Body1>
+        </GreenButton>
+      </View>
+    </>
   );
 };
 const styles = StyleSheet.create({
   container: {
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
-    borderWidth: 1,
+    // borderWidth: 1,
     backgroundColor: "#FFFFFF",
     paddingVertical: "12%",
     paddingHorizontal: "5%",
     borderRadius: 40,
     display: "flex",
     alignItems: "center",
+    position: "relative",
+    bottom: "17%",
     // justifyContent: "space-evenly",
 
     // flex: 2,
+  },
+  image: {
+    width: responsiveScreenWidth(100),
+    height: responsiveScreenHeight(46),
   },
 });
 export default Login;
