e09763e851b16b152ae267ef5c164f94654be709
Chaitanya Joshi <Chaitanya99j@gmail.com>
2022-12-12 10:02:13 +0530

    Adds Reward Card Scrren Template/UI

--- src/Screens/RewardCard.tsx
+++ src/Screens/RewardCard.tsx
@@ -6,8 +6,9 @@ import {
   ScreenContainer,
   WhiteRoundedContainer,
 } from "../styledComponents/Receipts,Reward,BillPage";
-import { Body1, Body5, Heading5 } from "../atoms/Typography";
+import { Body1, Body2, Body4, Body5, Heading5 } from "../atoms/Typography";
 import { responsiveScreenFontSize } from "react-native-responsive-dimensions";
+import { GreenButton } from "../atoms/GreenButton";
 
 const RewardCard = () => {
   return (
@@ -16,14 +17,19 @@ const RewardCard = () => {
       <WhiteRoundedContainer>
         <OptionsContainer style={styles.OptionsContainer}>
           <Heading5 style={styles.ContentHeader}>Receipts</Heading5>
-          <ImageBackground
-            resizeMode="cover"
-            source={require("../../assets/images/QRcode1x.png")}
-            style={styles.QRimage}
-          />
-          <Body5>
+          <View style={styles.ImageContainer}>
+            <ImageBackground
+              resizeMode="cover"
+              source={require("../../assets/images/QRcode3x.png")}
+              style={styles.QRimage}
+            />
+          </View>
+          <Body4 style={styles.TextBelowQRcode}>
             Please scan the barcode at the Counter to redeem the rewards{" "}
-          </Body5>
+          </Body4>
+          <GreenButton width={"95%"} height={"10%"} marginTop={"10%"}>
+            <Body2 style={styles.ButtonText}>View in Apple Wallet</Body2>
+          </GreenButton>
         </OptionsContainer>
       </WhiteRoundedContainer>
     </ScreenContainer>
@@ -33,9 +39,14 @@ const RewardCard = () => {
 export default RewardCard;
 
 const styles = StyleSheet.create({
-  OptionsContainer: { borderWidth: 1 },
+  OptionsContainer: {
+    // justifyContent: "space-even",
+    alignItems: "center",
+    //   borderWidth: 1,
+  },
   ScreenContainer: {
-    borderWidth: 1,
+    alignItems: "center",
+    //   borderWidth: 1,
   },
   ScreenName: {
     color: "white",
@@ -48,8 +59,27 @@ const styles = StyleSheet.create({
     // fontSize: "28",
   },
   QRimage: {
-    height: "50%",
-    width: "80%",
-    borderWidth: 1,
+    height: 325,
+    width: 325,
+    alignSelf: "center",
+    flex: 1,
+    // borderWidth: 1,
+  },
+  TextBelowQRcode: {
+    color: "#828282",
+    lineHeight: 25,
+    textAlign: "center",
+  },
+  ImageContainer: {
+    width: "100%",
+    height: "60%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "red",
+    //   borderWidth: 1,
+  },
+  ButtonText: {
+    color: "white",
+    fontWeight: "700",
   },
 });
