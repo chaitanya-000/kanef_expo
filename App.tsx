09ffbb2c419de5c2acede7e22f5fcd2a7328e51b
Chaitanya Joshi <Chaitanya99j@gmail.com>
2022-12-29 15:54:31 +0530

    removes header from both stack and tabs navigation

--- App.tsx
+++ App.tsx
@@ -9,6 +9,8 @@ import RewardList from "./src/Screens/RewardList";
 import Settings from "./src/Screens/Settings";
 import TabsNavigator from "./src/organisms/TabsNavigator";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Image } from "react-native";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -17,23 +19,63 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="RegisterPage" component={Register} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Settings" component={TabsNavigator} />
-        <Stack.Screen name="My Receipts" component={TabsNavigator} />
-        <Stack.Screen name="RewardLists" component={TabsNavigator} />
-        <Stack.Screen name="RewardCard" component={TabsNavigator} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="RegisterPage"
+          component={Register}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Settings"
+          component={TabsNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="My Receipts"
+          component={TabsNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="RewardLists"
+          component={TabsNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="RewardCard"
+          component={TabsNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Camera"
+          component={TabsNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
-
-{
-  /* <Tab.Navigator>
-        <Tab.Screen name="RewardCard" component={RewardCard} />
-        <Tab.Screen name="MyReceipts" component={MyReceipts} />
-        <Tab.Screen name="Settings" component={Settings} />
-        <Tab.Screen name="RewardList" component={RewardList} />
-      </Tab.Navigator> */
 }
--- src/organisms/TabsNavigator.tsx
+++ src/organisms/TabsNavigator.tsx
@@ -5,14 +5,47 @@ import Settings from "../Screens/Settings";
 import MyReceipts from "../Screens/MyReceipts";
 import RewardList from "../Screens/RewardList";
 import RewardCard from "../Screens/RewardCard";
+import Camera from "../Screens/Camera";
 
 const Tab = createBottomTabNavigator();
 const TabsNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="RewardCard" component={RewardCard} />
-      <Tab.Screen name="MyReceipts" component={MyReceipts} />
-      <Tab.Screen name="RewardList" component={RewardList} />
+      <Tab.Screen
+        name="RewardCard"
+        component={RewardCard}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="MyReceipts"
+        component={MyReceipts}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Camera"
+        component={Camera}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="RewardList"
+        component={RewardList}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          headerShown: false,
+        }}
+      />
     </Tab.Navigator>
   );
 };
