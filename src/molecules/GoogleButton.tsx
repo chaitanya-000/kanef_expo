a5f54be19ca7c68dbeaa6dbc43900f64f31417fd
Chaitanya Joshi <Chaitanya99j@gmail.com>
2023-05-22 22:31:38 +0530

    fix(googlebutton): fixes the height for the google button for android platform
    This commit fixes the height for andorid that was conditionally rendered depending on the platform
    as 6.4%, which made the button small. Solved it by increasint he height to 36.4%

--- src/molecules/GoogleButton.tsx
+++ src/molecules/GoogleButton.tsx
@@ -1,10 +1,97 @@
-import { TouchableOpacity, Image, Platform, StyleSheet } from "react-native";
-import React from "react";
-import { Body1 } from "../atoms/Typography";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Platform,
+  StyleSheet,
+} from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import {
+  Body1,
+  Body2,
+  Body3,
+  Body6,
+  Heading4,
+  Heading6,
+} from "../atoms/Typography";
+import { AuthContext } from "../store";
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function GoogleButton() {
+  const [token, setToken] = useState<string>();
+  const [userInfo, setUserInfo] = useState<any>();
+  const { setIsLoggedIn } = useContext(AuthContext);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId:
+      "1026449685475-u2e04rjo1g0h3uibjqbi1lprisarcjrk.apps.googleusercontent.com",
+    iosClientId:
+      "1026449685475-vtbgq4tnqh7ecl9m38evrm1rvve9itot.apps.googleusercontent.com",
+    expoClientId:
+      "1026449685475-7h9steioj713bls2dh72dj08jvpdcv40.apps.googleusercontent.com",
+  });
+
+  WebBrowser.maybeCompleteAuthSession();
+  const sendData = () => {
+    axios
+      .post("https://kenaf.ie/GoogleAPI", {
+        email: userInfo.email,
+        firstName: userInfo.given_name,
+        lastName: userInfo.family_name,
+        google_id: userInfo.id,
+      })
+      .then(async (response) => {
+        console.log(response.data);
+        if (response.data) {
+          await AsyncStorage.setItem(
+            "token",
+            JSON.stringify(response.data.user.google_id)
+          );
+        }
+        setIsLoggedIn(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    if (response?.type === "success") {
+      setToken(response.authentication.accessToken);
+      getUserInfo();
+    }
+  }, [response, token]);
+
+  const getUserInfo = async () => {
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+      setUserInfo(user);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // userInfo?.verified_email && sendData();
   return (
-    <TouchableOpacity style={styles.button}>
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => {
+        promptAsync({
+          useProxy: true,
+          showInRecents: true,
+        });
+      }}
+    >
       <Image source={require("../../assets/images/GoogleLogo.png")} />
       <Body1 style={{ color: "white", marginLeft: "6%" }}>Google</Body1>
     </TouchableOpacity>
@@ -37,4 +124,3 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 15,
   },
 });
-//cjID=111278293929217302926
