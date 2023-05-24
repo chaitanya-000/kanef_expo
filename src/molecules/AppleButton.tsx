f235aed61130fc7cf3839c11f23580ef10e1ff1c
Chaitanya Joshi <Chaitanya99j@gmail.com>
2023-05-24 18:34:27 +0530

    feat(applebutton): adds apple button authentication for testing on actual IOS device
    This commmit adds a testing functionality for actual IOS device. Currently in the emulator it's only
    showing data for the first time i.e, only when the share mail window pop's up. And that window only
    pops up when the expo app data and permissions are removed.

--- src/molecules/AppleButton.tsx
+++ src/molecules/AppleButton.tsx
@@ -1,28 +1,76 @@
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  Alert,
+} from "react-native";
 import React from "react";
 import { Body1 } from "../atoms/Typography";
+import * as AppleAuthentication from "expo-apple-authentication";
 
 export default function AppleButton() {
-  return (
-    <TouchableOpacity
-      style={{
-        borderRadius: 2,
-        width: "42%",
-        height: "37%",
-        backgroundColor: "#121F27",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        borderTopStartRadius: 15,
-        borderBottomEndRadius: 15,
-        borderBottomLeftRadius: 15,
-        borderTopRightRadius: 15,
-      }}
-    >
-      <Image source={require("../../assets/images/AppleLogo.png")} />
+  const handleAppleSignIn = async () => {
+    try {
+      const credential: any = await AppleAuthentication.signInAsync({
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+          AppleAuthentication.AppleAuthenticationScope.EMAIL,
+        ],
+      });
+      // Handle the signed-in user's credential
+      console.log(credential);
+      // Access user's name and email
 
-      <Body1 style={{ color: "white", marginLeft: "6%" }}>Apple</Body1>
-    </TouchableOpacity>
+      Alert.alert("Success", `Welcome, ${credential?.fullName.givenName}`);
+    } catch (error: any) {
+      if (error.code === "ERR_CANCELED") {
+        // User canceled the sign-in flow
+        Alert.alert("Cancelled", "Sign-in process was cancelled.");
+      } else {
+        // Handle other errors
+        Alert.alert("Error", "An error occurred during sign-in.");
+      }
+    }
+  };
+
+  return (
+    <AppleAuthentication.AppleAuthenticationButton
+      buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
+      buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
+      cornerRadius={24}
+      style={styles.button}
+      onPress={handleAppleSignIn}
+    />
+    // <TouchableOpacity
+    //   style={{
+    //     borderRadius: 15,
+    //     width: "42%",
+    //     height: "37%",
+    //     backgroundColor: "#121F27",
+    //     display: "flex",
+    //     flexDirection: "row",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <Image source={require("../../assets/images/AppleLogo.png")} />
+
+    //   <Body1 style={{ color: "white", marginLeft: "6%" }}>Apple</Body1>
+    // </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 15,
+    width: "42%",
+    height: "37%",
+    backgroundColor: "black",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
